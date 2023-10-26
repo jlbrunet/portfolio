313a2cd7ef64bf5c8c2c8c65dc26ceb157dfa726
Julie Brunet <juliebrunet21@msn.com>
2023-10-26 19:27:51 +0200

    background in progess - potato form in progress - check 1500 and window width inside the UseEffect

--- src/App.js
+++ src/App.js
@@ -1,36 +1,10 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
+import PotatoForm from "./Components/PotatoForm"
 
 function App() {
-  const maxWidth = 28
-  const maxHeight = 24
-
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-  const [divSize, setSize] = useState({width: `${maxWidth}rem`, height: `${maxHeight}rem`})
-
-  const updateWindowSize = () => {
-    setWindowWidth(window.innerWidth)
-  }
-
-  useEffect(() => {
-    window.addEventListener('resize', updateWindowSize);
-    return () => {
-      window.removeEventListener('resize', updateWindowSize);
-    };
-  })
-
-  useEffect(() => {
-    const percentage = (windowWidth / 1500)
-    const formSize = {width: `${percentage * maxWidth}rem`, height: `${percentage * maxHeight}rem`};
-    setSize(formSize)
-  }, [windowWidth])
-
-  const formShapeOne = {
-    borderRadius: "30% 40% 60% 40% / 70% 50% 50% 30%"
-  };
-
   return (
     <div className="w-screen h-screen bg-green relative">
-      <div style={{...formShapeOne, ...divSize}} className="bg-lime absolute top-1/4 left-1/4"></div>
+      <PotatoForm maxWidth="28" maxHeight="24" positionTop="top-1/4" positionLeft="left-1/4" borderRadius="30% 40% 60% 40% / 70% 50% 50% 30%"/>
     </div>
   );
 }
--- src/Components/PotatoForm.js
+++ src/Components/PotatoForm.js
@@ -1,9 +1,36 @@
 import React, {useEffect, useState} from "react";
 
-function PotatoForm() {
+function PotatoForm(props) {
+  const maxWidth = props.maxWidth
+  const maxHeight = props.maxHeight
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [divSize, setSize] = useState({width: `${maxWidth}rem`, height: `${maxHeight}rem`})
+
+  const updateWindowSize = () => {
+    setWindowWidth(window.innerWidth)
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', updateWindowSize);
+    return () => {
+      window.removeEventListener('resize', updateWindowSize);
+    };
+  })
+
+  useEffect(() => {
+    const percentage = (windowWidth / 1500)
+    const formSize = {width: `${percentage * maxWidth}rem`, height: `${percentage * maxHeight}rem`};
+    setSize(formSize)
+  }, [windowWidth])
+
+  const formShapeOne = {
+    borderRadius: props.borderRadius
+  };
+
   return(
-    <div></div>
+    <div style={{...formShapeOne, ...divSize}} className={`bg-lime absolute ${props.positionTop} ${props.positionLeft}`}></div>
   )
 }
 
-export default PotatoForm()
+export default PotatoForm
