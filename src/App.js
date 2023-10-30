5c107aae75786129c67359f47453d33f820fe42e
Julie Brunet <juliebrunet21@msn.com>
2023-10-30 18:59:39 +0100

    scrollbar personnalisée

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Presentation from "./Components/Presentation/Presentation";
 import Background from "./Components/Background";
+import About from "./Components/About";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="w-full h-full flex items-center justify-center">
         <Presentation />
       </div>
+      <About />
     </div>
   );
 }
---
+++ src/Components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className='w-full h-full'>
+      <div>Coucou</div>
+    </div>
+  );
+}
+
+export default About;
