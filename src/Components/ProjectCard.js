85ec405108bbfe7e15c4b6c8d3c4aa8db1ee8f5d
Julie Brunet <juliebrunet21@msn.com>
2023-11-03 19:43:35 +0100

    contenu en cours

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -5,12 +5,14 @@ const ProjectCard = (props) => {
   const { linkVideo, title, description, languages, linkCode } = props
 
   return (
-    <div className='rounded-custom5 bg-purple w-96 h-[30rem] p-5'>
-      <p className='w-4/5 h-2/5 bg-black m-auto'>{linkVideo}</p>
-      <p>{title}</p>
-      <p>{description}</p>
-      <p>{languages}</p>
-      <p>{linkCode}</p>
+    <div className='rounded-3xl bg-purple w-[20rem] h-[30rem]'>
+      <p className='w-full h-[45%] bg-white m-auto rounded-md'>{linkVideo}</p>
+      <div className='w-full px-5 py-1 m-auto bg-black'>
+        <p className='text-center text-2xl p-1'><b>{title}</b></p>
+        <p className=''>{description}</p>
+        <p className=''>{languages}</p>
+        <div className='text-center'><button>{linkCode}</button></div>
+      </div>
     </div>
   );
 }
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -7,7 +7,7 @@ const Projects = () => {
     <div className='flex flex-col justify-between'>
       <p className='w-full text-center text-6xl p-10 mb-10'>Mes projets</p>
       <div className='flex flex-wrap justify-around px-20'>
-        <div className='basis-1/2 flex justify-center p-6'><ProjectCard linkVideo ="tbd" title="Mon Portfolio" description="tbd" languages="tdb" linkCode="tdb"/></div>
+        <div className='basis-1/2 flex justify-center p-6'><ProjectCard linkVideo ="tbd" title="Mon Portfolio" description="Si tu es arrivé.e jusque-là c'est que tu n'as pas besoin de description ;)" languages="tdb" linkCode="tdb"/></div>
         <div className='basis-1/2 flex justify-center p-6'><ProjectCard linkVideo ="tbd" title="Paris Impro" description="tbd" languages="tdb" linkCode="tdb"/></div>
         <div className='basis-1/2 flex justify-center p-6'><ProjectCard linkVideo ="tbd" title="My Only Sub" description="tbd" languages="tdb" linkCode="tdb"/></div>
         <div className='basis-1/2 flex justify-center p-6'><ProjectCard linkVideo ="tbd" title="On Air" description="tbd" languages="tdb" linkCode="tdb"/></div>
