1685e96b997f25eb31a57fc72a186f933892fd0a
Julie Brunet <juliebrunet21@msn.com>
2023-11-06 17:29:18 +0100

    remplissage données des projets

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBrands } from '@fortawesome/free-solid-svg-icons'
+import { faSquareJs } from '@fortawesome/free-solid-svg-icons'
 
 const ProjectCard = (props) => {
 
